6a5edaed43dae8fd8cac21cc81840195bce90a71
Pedro Henrique Bazotti da Silva <101603901+eoBaza@users.noreply.github.com>
2023-07-17 00:43:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// window.sr = ScrollReveal({ reset: true});
+
+// sr.reveal('.flex', {
+//     duration: 1500,
+//     // distance: '150%',
+//     origin: 'left',
+//     opacity: 0,
+// });
+
+// sr.reveal('.textRev', {
+//     duration: 1500,
+//     // distance: '150%',
+//     origin: 'left',
+//     opacity: 0,
+// })
